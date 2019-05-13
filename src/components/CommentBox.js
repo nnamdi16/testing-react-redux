befe0bb5e5d1aa675ffcfb5b8039c446c6e5ddfc
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-13 16:11:44 +0100

    Create a handleChange method and a  to handle onChange and onSubmit event respectively

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,11 +5,24 @@ class CommentBox extends Component {
 		comment: ''
 	};
 
+	handleChange = e => {
+		this.setState({
+			comment: e.target.value
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		this.setState({
+			comment: ''
+		});
+	};
+
 	render() {
 		return (
-			<form>
+			<form onSubmit={this.handleSubmit}>
 				<h4>Add a Comment</h4>
-				<textarea value={this.state.comment} />
+				<textarea value={this.state.comment} onChange={this.handleChange} />
 				<div>
 					<button>Submit Comments</button>
 				</div>
