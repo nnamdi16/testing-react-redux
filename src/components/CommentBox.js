ab2a219f7aef705ef4ea1035ff769ba580b0f50c
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-13 12:27:07 +0100

    Create a CommentBox component that houses a form used for creating components

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-	return <div>CommentBox</div>;
-};
+class CommentBox extends Component {
+	state = {
+		comment: ''
+	};
+
+	render() {
+		return (
+			<form>
+				<h4>Add a Comment</h4>
+				<textarea value={this.state.comment} />
+				<div>
+					<button>Submit Comments</button>
+				</div>
+			</form>
+		);
+	}
+}
+
+export default CommentBox;
