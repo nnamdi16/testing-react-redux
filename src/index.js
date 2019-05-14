e127057c6fd62089e654c39838be9bed342b3f9a
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-14 22:11:07 +0100

    import materialize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import App from 'components/App';
 import * as serviceWorker from './serviceWorker';
 import Root from 'Root';
